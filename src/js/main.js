cef43b27a2c2fbf2fd898740ba16ee0a428824a9
Lelouchski <valentin.sepre@gmail.com>
2024-01-23 16:36:40 +0100

    1

--- src/js/main.js
+++ src/js/main.js
@@ -165,45 +165,10 @@ async function getSuperheroes() {
   
   const randomIndex = Math.floor(Math.random() * computerSelection.length);
 
-/*****************Valentin********** */
-
-const betInput = document.getElementById("input-bet");
-  const btnBet = document.getElementById("btnBet");
-  const coinValue = document.getElementById("coin");
-
-  coin = parseInt(localStorage.getItem("coin")) 
-  coinValue.textContent = coin;
-
-  btnBet.addEventListener("click", function () {
-    const input = parseInt(betInput.value);
-
-    if (input > coin || coin === 0) {
-      console.log("Vous n'avez pas assez d'argent !");
-    } else {
-      coin -= input;
-      coinValue.textContent = coin;
-      localStorage.setItem("coin", coin);
-      window.location.href = "game.html";
-    }
-  });
 
 
-// async function recuperateComputer() {
-//   try {
-//     const computerImg = document.getElementById("computer-img");
-//     if (computerImg) {
-//       const url = `https://superheroapi.com/api/${apiKey}/${computerSelection[randomIndex]}`;
-//       const result = await fetch(url);
-//       const resp = await result.json();
-//       computerImg.src = resp.image.url;
-//     } else {
-//       console.error("Aucun personnage sélectionné.");
-//     }
-//   } catch (error) {
-//     console.log("Erreur lors de la récupération du personnage", error);
-//   }
-// }
-//* Setting a to save the player ID to local storage
+
+
 const selectedPlayerId = localStorage.getItem("playerId")
 
 
