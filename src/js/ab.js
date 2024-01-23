2fbb0e66073ce8dc4728bbbfbf3a435c676efbcf
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2024-01-23 16:20:15 +0100

    modified

--- src/js/ab.js
+++ src/js/ab.js
@@ -17,13 +17,14 @@ let strength = document.getElementById("strength")
 const fightBtn = document.getElementById("btnFight")
 const selectedId = parseInt(localStorage.getItem("playerId"))
 const computerSelection = superheroIds.filter((id)=> id !== selectedId)
-const randomIndex = Math.floor(Math.random() * computerSelection.length);
 
 fightBtn.addEventListener("click", recuperateComputer)
    async function recuperateComputer() {
       try {
         const computerImg = document.getElementById("computer-img");
         if (computerImg) {
+            const randomIndex = Math.floor(Math.random() * computerSelection.length);
+
           const url = `https://superheroapi.com/api/${apiKey}/${computerSelection[randomIndex]}`;
           const result = await fetch(url);
           const resp = await result.json();
