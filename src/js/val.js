f7f0756088b323111983bc5fbf41c46a1c3c92cf
Lelouchski <valentin.sepre@gmail.com>
2024-01-24 10:56:17 +0100

    m

--- src/js/val.js
+++ src/js/val.js
@@ -3,24 +3,31 @@ document.getElementById("coin").innerHTML = coin;
 const betInput = document.getElementById("input-bet");
 const coinValue = document.getElementById("coin");
 const bet = document.getElementById("btnBet")
+
 bet.addEventListener("click", function () {
     const input = parseInt(betInput.value);
 
-    if (input > coin || coin === 0) {
+    if (!input) {
+        console.log("Vous n'avez pas misé");
+    } else if (input > coin || coin === 0) {
         console.log("Vous n'avez pas assez d'argent !");
     } else {
         coin -= input;
         coinValue.textContent = coin;
 
         localStorage.setItem("coin", coin);
-
         window.location.href = "game.html";
     }
 });
 
 function afficherMessage(){
     const resultMessage = document.getElementById("resultMessage")
-    
+    if (estGagnant) {
+        resultMessage.textContent = "Congratulations! You are the winner!";
+    } else {
+        resultMessage.textContent = "Looooooooser!";
+    }
 }
+
 
 
