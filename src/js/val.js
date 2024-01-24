9bd2fddbf4a54cbc512cbb79fac7d530f534cb0d
Lelouchski <valentin.sepre@gmail.com>
2024-01-24 11:03:59 +0100

    m

--- src/js/val.js
+++ src/js/val.js
@@ -1,21 +1,21 @@
-// let coin = parseInt(localStorage.getItem("coin")) || 100;
-// document.getElementById("coin").innerHTML = coin;
-// const betInput = document.getElementById("input-bet");
-// const coinValue = document.getElementById("coin");
-// // const bet = document.getElementById("btnBet")
+let coin = parseInt(localStorage.getItem("coin")) || 100;
+document.getElementById("coin").innerHTML = coin;
 const betInput = document.getElementById("input-bet");
-const btnBet = document.getElementById("btnBet");
 const coinValue = document.getElementById("coin");
 const bet = document.getElementById("btnBet")
+
+if (parseInt(localStorage.getItem("coin")))
+{
+coin = parseInt(localStorage.getItem("coin")) 
+coinValue.textContent = coin;
+}
+else
+{
+coinValue.textContent = coin;
+}
+
 bet.addEventListener("click", function () {
     const input = parseInt(betInput.value);
-    if (!input)
-    {
-      console.log("Vous n'avez pas misé !");
-    }
-    else 
-    {
-    localStorage.setItem("bet", input);
 
     if (!input) {
         console.log("Vous n'avez pas misé");
@@ -26,9 +26,9 @@ bet.addEventListener("click", function () {
         coinValue.textContent = coin;
 
         localStorage.setItem("coin", coin);
+
         window.location.href = "game.html";
     }
-  }
 });
 
 function afficherMessage(){
